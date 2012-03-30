fff6e84cae7356b3deaa613623d94ed19fcf6804
Julian.zhang <flesharcher@hotmail.com>
2012-03-30 22:29:01 +0800

    no message

--- DataMining/DecisionTree/c4.5.hs
+++ DataMining/DecisionTree/c4.5.hs
@@ -1,5 +1,15 @@
 import Data.Tree
 
+getDiv::Int->Int->Float
+getDiv p n= (fromInteger px)/(fromInteger nx)
+  where
+    px = toInteger p
+    nx = toInteger n
 
-
-
+getGain xt = pt*pf
+  where
+    countt = length $ filter (\x -> x) xt
+    pt = getDiv countt countAll
+    pf = getDiv countf countAll
+    countAll =  length $ xt
+    countf = countAll - countt
