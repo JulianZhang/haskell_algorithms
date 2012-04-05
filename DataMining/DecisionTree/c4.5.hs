e365d769aa8208e0c7425ca74d4e77ea7739eae3
Julian.zhang <flesharcher@hotmail.com>
2012-04-05 21:27:57 +0800

    enhance the gain ratio

--- DataMining/DecisionTree/c4.5.hs
+++ DataMining/DecisionTree/c4.5.hs
@@ -13,17 +13,24 @@ getEntropy xt = sum $ map (\x -> logTemp x) pList
     countList = map (\x -> length x) groupList
     countAll =  length xt
     pList = map (\x -> getDiv x countAll) countList
-    logTemp x = x*(logBase 2 x)
+
+logTemp x = x*(logBase 2 x)
 
 -- getAllGain::Ord a =>[([a],b)]->[[a]]
-getAllGain sList =  map (\x -> (sum x)/(toFloat all)) baseGainList  
+getAllGain sList = zipWith (/) gainList (getSplitInfox pList all)  
   where
     propsList = filter (\x ->(not.null) x) $ getProps $ map (\x -> fst x) sList
     valueList = map (\x -> snd x) sList
     zipList = map (\x -> zip x valueList) propsList
     pList = map (\x -> (groupBy fstGroup) (sortBy fstSort x) ) zipList
     all = sum $ map (\x -> length x) $ head pList
+    gainList = map (\x -> (sum x)/(toFloat all)) baseGainList 
     baseGainList = map (\x -> map (\y -> propGain y ) x) pList
+    
+
+getSplitInfox pList all = map (\x ->sum (getOneSplitInfo  x all )) pList
+    
+getOneSplitInfo vlist all = map (\y -> logTemp (getDiv (length y) all) ) vlist
     
 fstGroup x y= (fst x)==(fst y)
 
