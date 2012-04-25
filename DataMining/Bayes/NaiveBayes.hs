85169779029bce6195a39866b0f3f20097f62af5
Julian.zhang <flesharcher@hotmail.com>
2012-04-25 22:08:50 +0800

    getAllCount but not build the check function

--- DataMining/Bayes/NaiveBayes.hs
+++ DataMining/Bayes/NaiveBayes.hs
@@ -5,14 +5,20 @@ import Data.List
 import Debug.Trace
 import Util.Util
 
-getAllProb sList = fList
+getAllProb sList = allCount
   where
     resultList = map snd sList
+    nubResultList = nub.sort $ resultList
+    nubParList = map (nub.sort) $ transpose $ map fst sList
     rpList = getRP resultList
     groupList = head.group.sort $ resultList
     fList = map (\x -> filter (\y ->  x == (snd y) )  sList ) groupList 
+    -- allCount = map (\x -> (filter (\y -> x == (snd y) ) sList) ) nubResultList 
+    allCount = map (\x -> getParListCount (filter (\y -> x == (snd y) ) sList)   nubParList ) nubResultList 
     
+getParListCount fl npl =  zipWith getParCount (transpose (map fst fl) ) npl
 
+getParCount allPar nPar = map (\x -> (,) x (elemIndices x allPar )) nPar
 
 getRP s = map (\x -> (,) (fst x) (getDiv (snd x) all)) countList
   where
