1384b33261a97a819f504d5dbbca1cdbb5ecb7a1
Julian.zhang <flesharcher@hotmail.com>
2012-05-31 07:06:23 +0800

    try to add fork pool

--- WebSpider/Base.hs
+++ WebSpider/Base.hs
@@ -11,6 +11,8 @@ import Data.Maybe
 import Network.URI
 import qualified Data.ByteString as B
 
+import Control.Concurrent
+
 myConn = connectMySQL defaultMySQLConnectInfo {
                         mysqlHost     = "127.0.0.1",
                         mysqlUser     = "sqluser",
@@ -25,9 +27,13 @@ addWebPage x = myExe sqlString $ mark2sql x
   where
     sqlString = "INSERT INTO new_schema.webpage (pagename,pagenum ,pageurl,sittag,keyword) values(?,?,?,?,?)"
 
-getPageByTitle til = myConn >>= \x -> quickQuery' x sqlStr [toSql til]
+getPageByTitle::B.ByteString -> IO [[SqlValue]]
+getPageByTitle til =do 
+  conn<- myConn
+  setChar conn
+  quickQuery' conn sqlStr [toSql til]
   where
-    sqlStr =  "select * from new_schema.webpage where pagename like ? "
+    sqlStr =  "select * from new_schema.webpage where pagename like ? and sittag <> \"1\""
 
 
 -- myExeM sqlStr value= myConn>>= (\m -> (m >>= preAndExc) >> (m >>= commit))
@@ -49,3 +55,11 @@ mark2sql x = map (map toSql ) x
 
 getBytePages x = 
   (simpleHTTP $  defaultGETRequest_ $ (fromJust . parseURI) x ) >>=getResponseBody::IO B.ByteString
+
+myForkPool f count timeout xs=do
+  tm <- newEmptyMVar
+
+poolFork f x m = do
+  modifyMVar_ m (\y ->liftM (\z -> y ++ [z]) myThreadId)
+  f x
+  modifyMVar_ m (\y ->liftM (\z ->dropWhile (==z) y ) myThreadId)
