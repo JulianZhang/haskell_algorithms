ead71e658be4ddf341f8c51383cf49d77efbcf16
Julian.zhang <flesharcher@hotmail.com>
2012-10-14 09:47:04 +0800

    突然发现FPTREE没有完成，补充了部分代码，但是依然处于未完成状态

--- DataMining/PatternMining.hs
+++ DataMining/PatternMining.hs
@@ -1,4 +1,5 @@
 import Data.List
+import Data.Tree
 
 apriori::Ord a =>[[a]]->Int->[([a],Int)]
 apriori llc min = apriAll llc min st st
@@ -31,6 +32,64 @@ apriAll llc min allStep curStep
 
 -- fptree entry
 -- fpTree llc min = 
+
+testLLC = [["a","b","c"],["b","d","a"],["c","a","d"]]
+treeRoot = Node ("",99) []
+testTree = Node ("",99) [Node ("b",5) [],Node ("d",4) []]
+
+buildFPTree llc tree [] = tree
+
+-- buildFPTree llc tree itemLists = 
+
+mainFPTree llc = map (\x -> ) sList
+  where
+    sList = getSortList llc
+
+sndSort x y
+  | (snd x) == (snd y) = EQ
+  | (snd x) < (snd y)  = GT
+  | otherwise = LT
+   
+getSortList llc = sortBy sndSort countList
+  where
+    allList = group $  sort $ concat llc
+    countList =  map (\x -> (head x,length x)) allList
+    
+
+addTreeNode trace addNodeList myTree
+  | null traceItem = Node rootValue (nextLevel)
+  | otherwise = Node rootValue (nextLevel ++ [Node traceNode []])
+  where
+    nextLevel = map addNewTreeNode (subForest myTree)
+    addNewTreeNode = addTreeNode newTrace addNodeList
+    newTrace = trace ++ [rootValue]
+    rootValue = (fst.rootLabel) myTree
+    traceItem = filter (\x -> listEq newTrace (fst x)) addNodeList
+    traceNode = (snd.head) traceItem
+
+listEq la lb
+  | length la ==length lb = and $ zipWith (==) la lb
+  | otherwise = False
+
+--listTreeNode::Tree a->[a]->[[a]]
+listTreeNode trace myTree  = nextLevel ++ [newTrace]
+  where
+    rootValue = (fst.rootLabel) myTree
+    nextLevel = concat $ map (listTreeNode newTrace) (subForest myTree)
+    newTrace = trace ++[rootValue]
+
+findPosition node llc treeList
+ |null (fst filterLLC) = nextCall
+ |otherwise = [(needFind,length (fst filterLLC))]
+  where
+    curTl = head treeList
+    nextTl = tail treeList
+    needFind = curTl ++ node
+    filterLLC = partition (\x -> and (map (\y-> elem y x) needFind) ) llc
+    nextCall = findPosition node (snd filterLLC) nextTl
+    
+    
+-- myFilter  y =  
 
 getItemLists llc = foldr getItemList []  llc
 
